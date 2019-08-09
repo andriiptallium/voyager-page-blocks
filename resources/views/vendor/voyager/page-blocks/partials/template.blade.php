628b08a27437257f1ce887cf7f32a034076c129c
Anton <antonshamrow@gmail.com>
2019-08-09 19:06:10 +0300

    fix bug duplicated template code

--- resources/views/vendor/voyager/page-blocks/partials/template.blade.php
+++ resources/views/vendor/voyager/page-blocks/partials/template.blade.php
@@ -1,299 +1,151 @@
-@extends('voyager::master')
+<li class="dd-item" data-id="{{ $block->id }}" id="block-id-{{ $block->id }}">
+    <i class="glyphicon glyphicon-sort order-handle"></i>
 
-@section('css')
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <div class="panel panel-bordered panel-info @if ($block->is_minimized == 1) panel-collapsed @endif">
+        <div class="panel-heading">
 
-    <style type="text/css">
-        /* Image field type */
-        .vpb-image-group label { display: block; }
-        .vpb-image-group img { float: left; width: 28% !important; margin-right: 2%; }
-        .vpb-image-group input[type=file] { float: left; width: 70%; }
+            <h3 class="panel-title">
+                <a
+                    class="panel-action panel-collapse-icon voyager-angle-up"
+                    data-toggle="block-collapse"
+                    style="cursor:pointer"
+                >
+                    {{ $template->name }}
+                    @if (!empty($template->description)) <span class="panel-desc"> {{ $template->description }}</span>@endif
+                </a>
+            </h3>
+            <div class="panel-actions">
+                <a class="panel-action voyager-resize-full" data-toggle="panel-fullscreen" aria-hidden="true"></a>
+            </div>
+        </div>
 
-        /* Toggle Button */
-        .toggle.btn {
-            box-shadow: 0 5px 9px -3px rgba(0,0,0,0.2);
-            border: 1px solid rgba(0,0,0,0.2) !important;
-        }
+        <div class="panel-body" @if ($block->is_minimized == 1) style="display:none" @endif>
+            <form role="form" class="form-edit-add" action="{{ route('voyager.page-blocks.update', $block->id) }}" method="POST"
+                  enctype="multipart/form-data">
+                {{ method_field("PUT") }}
+                {{ csrf_field() }}
 
-        /* Collapsed Panel */
-        .panel-collapsed .panel-body {
-            /* display: none; */
-        }
-        .panel-collapsed .panel-collapse-icon {
-            transform: rotate(180deg);
-        }
+                <div class="row">
+                    @foreach($template->fields as $row)
+                        @if ($row->partial === 'break')</div> <!-- /.row --><div class="row"> @continue @endif
 
-        /* Make Inputs a 'lil more visible */
-        select,
-        input[type="text"],
-        .panel-body .select2-selection {
-            border: 1px solid rgba(0,0,0,0.17)
-        }
+                    @php $options = $row; @endphp
 
-        /* Reorder */
-        .dd .dd-placeholder {
-            max-height: 61px;
-            margin-bottom: 22px;
-        }
-        .dd h3.panel-title,
-        .dd-dragel h3.panel-title {
-            padding-left: 55px;
-        }
-        .dd-dragel .panel-body,
-        .dd-dragging .panel-body {
-            display: none !important;
-        }
-        .order-handle {
-            z-index: 1;
-            position: absolute;
-            padding: 20px 15px;
-            background: rgba(255,255,255,0.2);
-            font-size: 15px;
-            color: #fff;
-            line-height: 20px;
-            box-shadow: inset -2px 0px 2px rgba(0,0,0,0.1);
-            cursor: move;
-        }
-    </style>
-@stop
+                    @php
+                        $dataTypeContent = $block;
+                        $dataTypeContent->setTranslatableFields(array_keys(get_object_vars($template->fields)));
 
-@php
-    $isModelTranslatable = is_bread_translatable($page);
-@endphp
+                    @endphp
 
-@section('page_title', 'Edit Page Content')
+                    <div class="@if (strpos($row->partial, 'rich_text_box') !== false)col-md-12 @else col-md-6 @endif">
+                        <div class="form-group">
+                            <label>{{ $row->display_name }}</label>
+                            @include('voyager::multilingual.input-hidden-bread-edit-add')
+                            @php
+                                /* For 'multiple images' field - pass through the ID to identify the specific field */
+                                $dataTypeContent->id = $row->field;
+                            @endphp
+                            @include($row->partial)
+                        </div> <!-- /.form-group -->
+                    </div> <!-- /.col -->
+                    @endforeach
+                </div> <!-- /.row -->
 
-@section('page_header')
-    <h1 class="page-title">
-        <i class="voyager-file-text"></i>
-        Edit Page Content
-    </h1>
-    @include('voyager::multilingual.language-selector')
-@stop
-
-@section('content')
-    <div class="page-content edit-add container-fluid">
-        <div class="row">
-            <div class="col-md-3 col-lg-2">
-                <div class="panel panel-bordered panel-primary">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">Add Block</h3>
-                        <div class="panel-actions">
-                            <a class="panel-collapse-icon voyager-angle-down" data-toggle="block-collapse" aria-hidden="true"></a>
-                        </div> <!-- /.panel-actions -->
-                    </div> <!-- /.panel-heading -->
-
-                    <div class="panel-body">
-                        <form role="form" action="{{ route('voyager.page-blocks.store', $page->id) }}" method="POST"
-                              enctype="multipart/form-data">
-                            {{ csrf_field() }}
-
+                <div class="well" style="padding-bottom:0; margin-bottom:10px">
+                    <div class="row">
+                        <div class="col-mg-6 col-lg-4">
                             <div class="form-group">
-                                <label for="type">Block Type</label>
-                                <select class="form-control" name="type" id="type">
-                                    <option value="">-- Select --</option>
-                                    <optgroup label="Developer Tools">
-                                        <option value="include">Developer Controller</option>
-                                    </optgroup>
-                                    <optgroup label="Block Templates">
-                                        @php $templates = config('page-blocks'); @endphp
-
-                                        @foreach ($templates as $path => $template)
-                                            <option value="template|{{ $path }}">
-                                                {{ $template['name'] }}
-                                            </option>
-                                        @endforeach
-                                    </optgroup>
+                                <label for="cache_ttl">Cache Time</label>
+                                <select name="cache_ttl" id="cache_ttl" class="form-control">
+                                    <option value="0" {{ $block->cache_ttl === 0 ? 'selected="selected"' : '' }}>
+                                        None / Off
+                                    </option>
+                                    <option value="5" {{ $block->cache_ttl === 5 ? 'selected="selected"' : '' }}>
+                                        5 mins
+                                    </option>
+                                    <option value="30" {{ $block->cache_ttl === 30 ? 'selected="selected"' : '' }}>
+                                        30 mins
+                                    </option>
+                                    <option value="60" {{ $block->cache_ttl === 60 ? 'selected="selected"' : '' }}>
+                                        1 Hour
+                                    </option>
+                                    <option value="240" {{ $block->cache_ttl === 240 ? 'selected="selected"' : '' }}>
+                                        4 Hours
+                                    </option>
+                                    <option value="1440" {{ $block->cache_ttl === 1440 ? 'selected="selected"' : '' }}>
+                                        1 Day
+                                    </option>
+                                    <option value="10080" {{ $block->cache_ttl === 10080 ? 'selected="selected"' : '' }}>
+                                        7 Days
+                                    </option>
                                 </select>
                             </div> <!-- /.form-group -->
+                        </div> <!-- /.col -->
 
-                            <input type="hidden" name="page_id" value="{{ $page->id }}"/>
-                            <button type="submit" class="btn btn-success btn-sm">{{ __('voyager::generic.add') }}</button>
-                        </form>
-                    </div> <!-- /.panel-body -->
-                </div> <!-- /.panel -->
-            </div> <!-- /.col -->
+                        <div class="col-mg-6 col-lg-8">
+                            <label>Options</label>
 
-            <div class="col-md-9 col-lg-10">
-                @if (count($errors) > 0)
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
+                            <div class="row">
+                                <div class="col-md-6 col-lg-5">
+                                    <div class="form-group">
+                                        <input
+                                            type="checkbox"
+                                            name="is_hidden"
+                                            id="is_hidden"
+                                            data-name="is_hidden"
+                                            class="toggleswitch"
+                                            value="1"
+                                            data-on="Yes" {{ $block->is_hidden ? 'checked="checked"' : '' }}
+                                            data-off="No"
+                                        />
+                                        <label for="is_hidden"> &nbsp;Hide Block</label>
+                                    </div> <!-- /.form-group -->
+                                </div> <!-- /.col -->
 
-                <div class="dd">
-                    <ol class="dd-list">
-                        @foreach($pageBlocks as $block)
-                            @php
-                                $template = $block->template();
-                                $dataTypeContent = $block->data;
-                            @endphp
+                                <div class="col-md-6 col-lg-5">
+                                    <div class="form-group">
+                                        <input
+                                            type="checkbox"
+                                            name="is_delete_denied"
+                                            id="is_delete_denied"
+                                            data-name="is_delete_denied"
+                                            class="toggleswitch"
+                                            value="1"
+                                            data-on="Yes" {{ $block->is_delete_denied ? 'checked="checked"' : '' }}
+                                            data-off="No"
+                                        />
+                                        <label for="is_delete_denied"> &nbsp;Prevent Deletion</label>
+                                    </div> <!-- /.form-group -->
+                                </div> <!-- /.col -->
+                            </div> <!-- /.row -->
+                        </div> <!-- /.col -->
+                    </div> <!-- /.row -->
+                </div> <!-- /.well -->
 
-                            @if ($block->type === 'template')
-                                @include('voyager::page-blocks.partials.template')
-                            @else
-                                @include('voyager::page-blocks.partials.include')
-                            @endif
-                        @endforeach
-                    </ol> <!-- /.dd-list -->
-                </div> <!-- /.dd -->
-            </div> <!-- /.col -->
-        </div> <!-- /.row -->
-    </div> <!-- /.page-content -->
-@stop
+                <span class="btn-group-lg">
+                    <button
+                        style="float:left"
+                        type="submit"
+                        class="btn btn-success btn-lg save"
+                    >{{ __('voyager::generic.save') }} This Block</button>
+                </span>
+            </form>
 
-@section('javascript')
-    <script>
-        $('document').ready(function () {
-            /**
-             * Enable CHECKBOX toggle component
-             */
-            $('.toggleswitch').bootstrapToggle();
+            @if (!$block->is_delete_denied)
+                <form method="POST" action="{{ route('voyager.page-blocks.destroy', $block->id) }}">
+                    {{ method_field("DELETE") }}
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-            /**
-             * Make TINYMCE a 'lil smaller, height-wise
-             */
-            setTimeout(function() {
-                $('.mce-tinymce').each(function() {
-                    $(this).find('iframe').css({'height': 250, 'min-height': 250});
-                });
-            }, 1000);
-
-            /**
-             * IMAGE fields types
-             */
-            $('input[type=file]').each(function() {
-                $(this).closest('.form-group').addClass('vpb-image-group');
-            });
-
-            /**
-             * MULTIPLE-IMAGES Delete function
-             */
-            /*$(".remove-multi-image").on('click', function(e){
-                e.preventDefault();
-                var result = confirm("Are you sure you want to delete this image?");
-                if (result) {
-                    $.post('{{-- route('voyager.page-blocks.delete-multiple-image') --}}', {
-                        field: $(this).data('id'),
-                        file_name: $(this).data('file-name'),
-                        _token: '{{ csrf_token() }}'
-                    });
-                }
-            });*/
-
-            /**
-             * Confirm DELETE block
-             */
-            $("[data-delete-block-btn]").on('click', function(e){
-                e.preventDefault();
-                var result = confirm("Are you sure you want to delete this block?");
-                if (result) $(this).closest('form').submit();
-            });
-
-            /**
-             * COLLAPSE blocks
-             */
-            // Init
-            $(document).on('click', '.panel-heading [data-toggle="block-collapse"]', function (e) {
-                e.preventDefault();
-
-                $(this).parents('.panel').toggleClass('panel-collapsed');
-                $(this).parents('.panel').find('.panel-body').slideToggle();
-
-                var minimized = 0;
-                if ($(this).parents('.panel').hasClass('panel-collapsed')) {
-                    minimized = 1;
-                }
-
-                $.post('{{ route('voyager.page-blocks.minimize') }}', {
-                    id: $(this).parents('li').data('id'),
-                    is_minimized: minimized,
-                    _token: '{{ csrf_token() }}'
-                });
-            });
-
-            /**
-             * ORDER blocks
-             */
-            // Init drag 'n drop
-            $('.dd').nestable({ handleClass: 'order-handle', maxDepth: 1 });
-
-            // Close all panels when dragging
-            $('.order-handle').on('mousedown', function() { $('.dd').addClass('dd-dragging'); });
-
-            // Fire request when drag complete
-            $('.dd').on('change', function (e) {
-                // Only when it's a result of drag and drop
-                // -- Otherwise this triggers on every form change within .dd
-                if ($('.dd').hasClass('dd-dragging')) {
-                    // And reopen panels once drag has finished
-                    $('.dd').removeClass('dd-dragging');
-
-                    // Post the request
-                    $.post('{{ route('voyager.page-blocks.sort') }}', {
-                        order: JSON.stringify($('.dd').nestable('serialize')),
-                        _token: '{{ csrf_token() }}'
-                    }, function (data) {
-                        toastr.success("Block order saved");
-                    });
-                }
-            });
-
-            @if ($isModelTranslatable)
-            $('.side-body').multilingual({"editing": true});
+                    <span class="btn-group-xs">
+                        <button
+                            data-delete-block-btn
+                            type="submit"
+                            style="float:right; margin-top:22px"
+                            class="btn btn-danger btn-xs delete"
+                        >{{ __('voyager::generic.delete') }} This Block</button>
+                    </span>
+                </form>
             @endif
-        });
-    </script>
-    <script>
-        $(document).ready(function () {
-            $("form").submit(function (e) {
-                document.getElementsByName('bullets')[0].value = bulletsEditor.getData();
-            });
-        });
-    </script>
-    <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
-    <script>
-
-        let bulletsEditor = CKEDITOR.replace('bullets');
-        const trafficElem = document.getElementsByName('bullets')[0];
-
-        trafficElem.addEventListener('change', (e) => {
-            bulletsEditor.setData(e.target.value);
-        });
-
-        document.querySelectorAll('.language-selector label').forEach(function(btn) {
-            btn.addEventListener('click', function() {
-                document.getElementsByName('bullets')[0].value = bulletsEditor.getData();
-            });
-        });
-
-        var registered = [];
-        var setDetectChangeHandler = function(field) {
-            if (!registered.includes(field)) {
-                var superProps = Object.getPrototypeOf(field);
-                var superSet = Object.getOwnPropertyDescriptor(superProps, "value").set;
-                var superGet = Object.getOwnPropertyDescriptor(superProps, "value").get;
-                var newProps = {
-                    get: function() {
-                        return superGet.apply(this, arguments);
-                    },
-                    set: function (t) {
-                        var _this = this;
-                        setTimeout( function() { _this.dispatchEvent(new Event("change")); }, 50);
-                        return superSet.apply(this, arguments);
-                    }
-                };
-                Object.defineProperty(field, "value", newProps);
-                registered.push(field);
-            }
-        };
-
-        setDetectChangeHandler(trafficElem);
-
-    </script>
-
-@endsection
+        </div> <!-- /.panel-body -->
+    </div> <!-- /.panel -->
+</li> <!-- /.dd-item -->
